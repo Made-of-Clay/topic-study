ea95e41f513dad28e116f253038e7e8c1ea3563a
Adam <adam.m.leis@gmail.com>
2017-11-22 17:57:21 -0500

    add vuex and topicService to Vue instance

--- src/main.js
+++ src/main.js
@@ -1,11 +1,22 @@
+"use strict";
+
 import Vue from 'vue';
+import Vuex from 'vuex';
 import VueMaterial from 'vue-material';
 import App from './App.vue';
-import axios from 'axios';
+import InitStore from './store.js';
+import TopicService from './Topic.service.js';
 
 Vue.use(VueMaterial);
+Vue.use(Vuex);
+Vue.use({
+    install(Vue) {
+        Vue.prototype.topicService = new TopicService();
+    }
+});
 
 new Vue({
     el: '#app',
-    render: h => h(App)
+    render: h => h(App),
+    store: new Vuex.Store(InitStore),
 });
